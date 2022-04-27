ab44b9eaf8a5b0f673f8d5be795a5f0e62e736d9
AmandaOvia <101339756+AmandaOvia@users.noreply.github.com>
2022-04-27 09:27:21 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    number1 += -1
+    basic.showNumber(number1)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("Hello!")
+    basic.showNumber(0)
+})
+input.onButtonPressed(Button.B, function () {
+    number1 += 1
+    basic.showNumber(number1)
+})
+input.onGesture(Gesture.Shake, function () {
+    number2 = number1
+})
+function tife4l (num: number, num2: number) {
+    sum = num + num2
+}
+let sum = 0
+let number2 = 0
+let number1 = 0
+number1 = 0
+basic.forever(function () {
+	
+})
